3c902863d0e7af32a5d50d1b21ce72f31304d788
Jeff Dederick <Xpressivmotion@gmail.com>
2018-01-19 12:04:07 -0500

    binary tree initial setup complete

---
+++ binaryTree.js
@@ -0,0 +1,50 @@
+const util = require('util');
+
+class BinaryNode {
+  constructor(left, right, data) {
+    this.left = left;
+    this.right = right;
+    this.data = data;
+  }
+}
+
+const rootNode = new BinaryNode(null, null, 7);
+
+const binary_tree = [8, 10, 3, 1, 6, 14, 4, 7, 13];
+
+class Tree {
+  constructor(rootNode) {
+    this.rootNode = rootNode;
+  }
+
+  addValue(value, currentNode = this.rootNode) {
+    currentNode.data > value
+      ? !currentNode.left
+        ? (currentNode.left = new BinaryNode(null, null, value))
+        : this.addValue(value, currentNode.left)
+      : !currentNode.right
+        ? (currentNode.right = new BinaryNode(null, null, value))
+        : this.addValue(value, currentNode.right);
+  }
+
+  removeNode(node) {}
+
+  findValue(node) {}
+
+  updateValue(node) {}
+
+  updateNode(node) {}
+
+  printTree() {
+    console.log(util.inspect(Birch, false, null));
+  }
+}
+
+const Birch = new Tree(rootNode);
+
+const hydrate = arr => {
+  arr.forEach(val => Birch.addValue(val));
+};
+
+hydrate(binary_tree);
+console.log(util.inspect(Birch, false, null));
